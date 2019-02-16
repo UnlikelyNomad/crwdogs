04927fa3a0a289e84b2dbf24c4cd9c5c1c587fe5
Derrick Bommarito <derrick.a.bommarito@gmail.com>
2019-02-16 11:19:50 -0600

    Hadn't saved signup file

--- www/signup/index.php
+++ www/signup/index.php
@@ -19,19 +19,6 @@ function echoIfPost($key) {
     }
 }
 
-$keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+ ';
-function random_str($length) {
-    global $keyspace;
-
-    $str = '';
-    $max = mb_strlen($keyspace, '8bit') - 1;
-
-    for ($i = 0; $i < $length; ++$i) {
-        $str .= $keyspace[random_int(0, $max)];
-    }
-    return $str;
-}
-
 $error = NULL;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['signup-submit'])) {
