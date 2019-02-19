bf4df026a7182c43c9aa63cce8ce9b62ae4d8efa
Derrick Bommarito <derrick.a.bommarito@gmail.com>
2019-02-18 23:55:34 -0600

    adding missing include

--- www/fix/regitemforevent.php
+++ www/fix/regitemforevent.php
@@ -2,6 +2,7 @@
 
 require_once '../../vendor/autoload.php';
 require_once '../../generated-conf/config.php';
+require_once '../common/crwdogs.inc.php';
 
 use \crwdogs\events\RegistrationQuery;
 use \crwdogs\events\PurchasedItem;
